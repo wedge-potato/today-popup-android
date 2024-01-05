0c2493d73ed21d70aae182ae6853b5bdf3f93d99
jgeun <pjk8926@gmail.com>
2024-01-05 23:50:31 +0900

    feat: add font

---
+++ app/src/main/java/com/wedge/todaypopup/core/ui/theme/FontFamily.kt
@@ -0,0 +1,15 @@
+package com.wedge.todaypopup.core.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.wedge.todaypopup.R
+
+val suit = FontFamily(
+    Font(R.font.suit_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.suit_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.suit_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.suit_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.suit_thin, FontWeight.Thin, FontStyle.Normal),
+)
--- app/src/main/java/com/wedge/todaypopup/core/ui/theme/Type.kt
+++ app/src/main/java/com/wedge/todaypopup/core/ui/theme/Type.kt
@@ -32,3 +32,66 @@ val Typography = Typography(
     )
     */
 )
+
+val Header1 = TextStyle(
+	fontFamily = suit,
+	fontWeight = FontWeight.Bold,
+	fontSize = 24.sp,
+	lineHeight = 32.sp
+)
+
+val Subtitle1 = TextStyle(
+	fontFamily = suit,
+	fontWeight = FontWeight.SemiBold,
+	fontSize = 18.sp,
+	lineHeight = 24.sp
+)
+
+val Subtitle2 = TextStyle(
+	fontFamily = suit,
+	fontWeight = FontWeight.SemiBold,
+	fontSize = 16.sp,
+	lineHeight = 24.sp
+)
+
+val Subtitle3 = TextStyle(
+	fontFamily = suit,
+	fontWeight = FontWeight.Medium,
+	fontSize = 12.sp,
+	lineHeight = 18.sp
+)
+
+val Body1 = TextStyle(
+	fontFamily = suit,
+	fontWeight = FontWeight.Normal,
+	fontSize = 16.sp,
+	lineHeight = 24.sp
+)
+
+val Body2 = TextStyle(
+	fontFamily = suit,
+	fontWeight = FontWeight.Medium,
+	fontSize = 14.sp,
+	lineHeight = 2.sp
+)
+
+val Caption1 = TextStyle(
+	fontFamily = suit,
+	fontWeight = FontWeight.SemiBold,
+	fontSize = 12.sp,
+	lineHeight = 18.sp
+)
+
+val Caption2 = TextStyle(
+	fontFamily = suit,
+	fontWeight = FontWeight.Bold,
+	fontSize = 10.sp,
+	lineHeight = 12.sp
+)
+
+val Caption3 = TextStyle(
+	fontFamily = suit,
+	fontWeight = FontWeight.SemiBold,
+	fontSize = 8.sp,
+	lineHeight = 10.sp
+)
