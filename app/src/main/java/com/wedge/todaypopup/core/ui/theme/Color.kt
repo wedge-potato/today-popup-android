f1bac208eaafb922fce3c668d33cbdd342b3f8ab
jgeun <pjk8926@gmail.com>
2024-01-05 23:50:39 +0900

    feat: add color system

--- app/src/main/java/com/wedge/todaypopup/core/ui/theme/Color.kt
+++ app/src/main/java/com/wedge/todaypopup/core/ui/theme/Color.kt
@@ -10,9 +10,35 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-
 val dark_mode_bg = Color(0xFF111111)
 val dark_mode_text = Color(0xFFFAFAFA)
 
 val light_mode_bg = Color(0xFFF9F9F9)
 val light_mode_text = Color(0xFF131313)
+
+val Coral10 = Color(0xFFFFEBED)
+val Coral20 = Color(0xFFFFCCCF)
+val Coral30 = Color(0xFFF89894)
+val Coral40 = Color(0xFFF06F6A)
+val Coral50 = Color(0xFFFA4D42)
+val Coral60 = Color(0xFFFF3C21)
+val Coral70 = Color(0xFFF02E22)
+val Coral80 = Color(0xFFDE211D)
+val Coral90 = Color(0xFFD21715)
+
+val Blue50 = Color(0xFF0073F9)
+val Red50 = Color(0xFFFF201E)
+val Yellow50 = Color(0xFFFFE500)
+
+val Grey05 = Color(0xFFFBFBFB)
+val Grey10 = Color(0xFFF6F6F6)
+val Grey20 = Color(0xFFF1F1F1)
+val Grey30 = Color(0xFFE6E6E6)
+val Grey40 = Color(0xFFC3C3C3)
+val Grey50 = Color(0xFFA5A5A5)
+val Grey60 = Color(0xFF7C7C7C)
+val Grey70 = Color(0xFF555555)
+val Grey80 = Color(0xFF1E1E1E)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
